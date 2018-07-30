489c14593b116755d59411739c46fd4c98efc2d4
weijhfly <1241968795@qq.com>
2018-07-30 17:49:34 +0800

    添加rolldate思维导图

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# rolldate
+## rolldate ([jquery-date--旧版路径](https://github.com/weijhfly/jqueryDatePlugin "jquery-date"))
 暂未完成
--- src/index.js
+++ src/index.js
@@ -1,28 +1,22 @@
 import './rolldate.less';
-var pkg = require('../package.json')
 // 从npm下载的iscroll发现无法滑动bug，这里用github上的最新版v5.2.0-snapshot
 import IScroll from './iscroll/iscroll';
+let pkg = require('../package.json');
 
-let c = 2;
+// 暂定dom只渲染一次
+console.log('正在渲染ui');
+var yearScroll = new IScroll(".rolldate-year", {
+   snap: 'li'
+});
+var monthScroll = new IScroll(".rolldate-month", {
+    snap: 'li'
+});
+var dayScroll = new IScroll(".rolldate-day", {
+    snap: 'li'
+});
 export class Date{
-	constructor(el, obj){
-		console.log(el,obj);
-        this.createUi();
-        this.setIscroll();
-    }
-    createUi(){
-    	console.log('正在渲染ui');
-    }
-    setIscroll(){
-    	var yearScroll = new IScroll(".rolldate-year", {
-    	       snap: 'li'
-			});
-		var monthScroll = new IScroll(".rolldate-month", {
-		    snap: 'li'
-		});
-		var dayScroll = new IScroll(".rolldate-day", {
-		    snap: 'li'
-		});
+	constructor(config){
+		console.log(config);
     }
 }
 export var version = pkg.version;
